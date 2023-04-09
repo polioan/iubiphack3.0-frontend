d3b810c6581dfac687b525a228de417e294cfd7c
User <user@1.1>
2023-04-09 17:12:32 +0300

    fixed profile data

--- src/hooks/useProfileQuery.ts
+++ src/hooks/useProfileQuery.ts
@@ -1,5 +1,5 @@
 import { useQuery } from '@tanstack/react-query'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import axios, { isAxiosError } from '../common/axios'
 import { error, uniqueToast } from '../common/toast'
 
@@ -8,6 +8,7 @@ export default function useProfileQuery() {
     ['/profile'],
     async () => {
       const { data } = await axios.post<App.ProfileResponce>('/profile')
+      console.log(data)
       return data
     },
     {
@@ -25,26 +26,35 @@ export default function useProfileQuery() {
     }
   )
 
-  const [name, setName] = useState(result?.data?.FirstName)
-  const [surname, setSurname] = useState(result?.data?.LastName)
-  const [middlename, setMiddlename] = useState(result?.data?.Surname)
-  const [phone, setPhone] = useState(result?.data?.PhoneNumber)
-  const [email, setEmail] = useState(result?.data?.Email)
+  const [name, setName] = useState('')
+  const [surname, setSurname] = useState('')
+  const [middlename, setMiddlename] = useState('')
+  const [phone, setPhone] = useState('')
+  const [email, setEmail] = useState('')
+
+  useEffect(() => {
+    if (!result.isSuccess) return
+    setName(result.data?.FirstName)
+    setSurname(result.data?.LastName)
+    setMiddlename(result.data?.Surname)
+    setPhone(result.data?.PhoneNumber)
+    setEmail(result.data?.Email)
+  }, [result.isSuccess])
 
   const initial = {
-    name: name ?? '',
+    name,
     setName: (e: React.ChangeEvent<HTMLInputElement>) =>
       setName(e.target.value),
-    surname: surname ?? '',
+    surname,
     setSurname: (e: React.ChangeEvent<HTMLInputElement>) =>
       setSurname(e.target.value),
-    middlename: middlename ?? '',
+    middlename,
     setMiddlename: (e: React.ChangeEvent<HTMLInputElement>) =>
       setMiddlename(e.target.value),
-    phone: phone ?? '',
+    phone,
     setPhone: (e: React.ChangeEvent<HTMLInputElement>) =>
       setPhone(e.target.value),
-    email: email ?? '',
+    email,
     setEmail: (e: React.ChangeEvent<HTMLInputElement>) =>
       setEmail(e.target.value),
   }
